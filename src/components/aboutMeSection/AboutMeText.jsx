da369ab50172bc8e50d65d413bf85f05f3222658
Tusshed <icloud4israel@gmail.com>
2025-02-24 15:08:29 -0500

    new changes

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -5,13 +5,14 @@ const AboutMeText = () => {
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p>
-        I’m Abdelrahman, a web developer instructor with a passion for teaching
-        and coding. I specialize in React and front-end development, helping
-        students build real-world projects and master modern web technologies. I
-        also run a YouTube channel, CodeNest, where I create tutorials and
-        courses to guide aspiring developers in their journey toward successful
-        careers in tech. Outside of coding, I enjoy continuous learning and
-        sharing knowledge to inspire others to achieve their goals.
+        Hi, I&lsquo;m Gaby, a passionate Software Analyst dedicated to bridging
+        the gap between business goals and technology solutions. With expertise
+        in system analysis, data-driven decision-making, and software
+        optimization, I help businesses streamline processes, enhance
+        efficiency, and make informed technical decisions. Whether its
+        requirement gathering, workflow optimization, or troubleshooting complex
+        systems, I bring a strategic approach to every project. Lets connect and
+        build smarter solutions together!.
       </p>
       <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
         <Link
