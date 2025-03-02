585a34c07bb4d2e42e8435d6598a9ec9cb42b83b
Tusshed <icloud4israel@gmail.com>
2025-03-02 13:48:57 -0500

    projects bullet points added

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -15,7 +15,11 @@ const projects = [
     align: "left",
     image: Img1,
     link: "#",
-    descriptionPoints: ["a point here", "a point here", "a point here"],
+    descriptionPoints: [
+      "Conducted performance tests to ensure system handles student traffic.",
+      "Collected feedback from students to improve the system.",
+      "Provided training for students and staff.",
+    ],
   },
   {
     name: "Real Estate Website Optimization",
@@ -23,15 +27,23 @@ const projects = [
     align: "right",
     image: Img2,
     link: "#",
-    descriptionPoints: ["something here", "a point here", "a point here"],
+    descriptionPoints: [
+      "Helped to create the visual look and feel of the page.",
+      "Simulated high-traffic scenarios to ensure platform stability.",
+      "Ensured the page's forms and data collection work correctly.",
+    ],
   },
   {
-    name: "Dashboard & Reporting System",
-    year: "xBusiness",
+    name: "Streaming & Content Delivery",
+    year: "CircuitTv",
     align: "left",
     image: Img3,
     link: "#",
-    descriptionPoints: ["something here", "a point here", "a point here"],
+    descriptionPoints: [
+      "Tested app features to determine which resonated best with users.",
+      "Verified content security to prevent piracy and unauthorized access.",
+      "Analyzed user engagements and preferences to improve content suggestions",
+    ],
   },
   {
     name: "Software Analysis & Optimization",
@@ -39,7 +51,11 @@ const projects = [
     align: "right",
     image: img4,
     link: "#",
-    descriptionPoints: ["something here", "a point here", "a point here"],
+    descriptionPoints: [
+      "Tested mobile app UI/UX for consistency and ease of use.",
+      "Assessed app performance with multiple connected bulbs.",
+      "Verified Wi-Fi/Bluetooth connectivity for proper bulb synchronization.",
+    ],
   },
   {
     name: "E-commerce Workflow Automation",
@@ -47,7 +63,11 @@ const projects = [
     align: "left",
     image: img5,
     link: "#",
-    descriptionPoints: ["something here", "a point here", "a point here"],
+    descriptionPoints: [
+      "Verified store creation, setup, and product catalog management.",
+      "Tested payment gateway and third-party integrations (PayPal, Stripe, credit cards).",
+      "Validated API functionality for real-time inventory updates and payments.",
+    ],
   },
 ];
 
